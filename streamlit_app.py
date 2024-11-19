c77ffb56a7a46e9b68328975223909378a83f963
sc-katu <107098571+sc-katu@users.noreply.github.com>
2024-11-19 22:39:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,10 @@ df =pd.read_excel("https://github.com/sc-katu/yieldprogramming/raw/main/2023_yie
 
 # Main content
 st.title('Программирование урожая с учетом фитосанитарных работ')
-st.set_option('deprecation.showPyplotGlobalUse', False)
+try:
+    st.set_option('global.deprecation.showPyplotGlobalUse', False)
+except streamlit.errors.StreamlitAPIException as e:
+    st.write(f"Configuration option not recognized: {e}")
 markdown_table = """
     | Уровень защиты                  | Описание                                       |
     |-----------------------|----------------------------------------------------|
